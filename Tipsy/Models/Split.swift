111c9098bb4122dd7951d8c739275d2373f6ec7a
ninanee <ni.yun@outlook.com>
2024-05-14 11:01:40 -0400

    finished the multi-screen project

---
+++ Tipsy/Models/Split.swift
@@ -0,0 +1,9 @@
+//
+//  Split.swift
+//  Tipsy
+//
+//  Created by Yun Ni on 2024-05-14.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Tipsy/Models/SplitBrain.swift
@@ -0,0 +1,9 @@
+//
+//  SplitBrain.swift
+//  Tipsy
+//
+//  Created by Yun Ni on 2024-05-14.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
